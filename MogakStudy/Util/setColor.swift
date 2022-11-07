b724d1f6384c38ddd27c0fb585917f62db7d2056
Doy Kim <kimdee.dev@gmail.com>
2022-11-08 01:57:38 +0900

    [Preceded] #1 커스텀 컬러 설정

---
+++ MogakStudy/Util/setColor.swift
@@ -0,0 +1,36 @@
+//
+//  setColor.swift
+//  MogakStudy
+//
+//  Created by Doy Kim on 2022/11/08.
+//
+
+import UIKit
+
+extension UIColor {
+    struct customColor {
+        // Black & White
+        static let white: UIColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        static let black: UIColor = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        
+        // Brand Color
+        static let brandGreen: UIColor = #colorLiteral(red: 0.2862745098, green: 0.862745098, blue: 0.5725490196, alpha: 1)
+        static let brandWhiteGreen: UIColor = #colorLiteral(red: 0.8039215686, green: 0.9568627451, blue: 0.8823529412, alpha: 1)
+        static let brandYellowGreen: UIColor = #colorLiteral(red: 0.6980392157, green: 0.9215686275, blue: 0.3803921569, alpha: 1)
+        
+        // Grayscale
+        static let gray001: UIColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
+        static let gray002: UIColor = #colorLiteral(red: 0.937254902, green: 0.937254902, blue: 0.937254902, alpha: 1)
+        static let gray003: UIColor = #colorLiteral(red: 0.8862745098, green: 0.8862745098, blue: 0.8862745098, alpha: 1)
+        static let gray004: UIColor = #colorLiteral(red: 0.8196078431, green: 0.8196078431, blue: 0.8196078431, alpha: 1)
+        static let gray005: UIColor = #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 1)
+        static let gray006: UIColor = #colorLiteral(red: 0.6666666667, green: 0.6666666667, blue: 0.6666666667, alpha: 1)
+        static let gray007: UIColor = #colorLiteral(red: 0.5333333333, green: 0.5333333333, blue: 0.5333333333, alpha: 1)
+        
+        // SystemColor
+        static let systemSuccess: UIColor = #colorLiteral(red: 0.3843137255, green: 0.5607843137, blue: 0.8980392157, alpha: 1)
+        static let systemError: UIColor = #colorLiteral(red: 0.9137254902, green: 0.4, blue: 0.4196078431, alpha: 1)
+        static let systemFocus: UIColor = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    }
+}
+
