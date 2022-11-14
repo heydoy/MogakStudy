a6592e82e5e9c87134bf391855d22a6a9da734ee
Doy Kim <kimdee.dev@gmail.com>
2022-11-15 05:54:39 +0900

    [Refactor] 폰트명을 열거형으로 정리하여 추가, 온보딩용 폰트 추가

--- MogakStudy/Util/setFont.swift
+++ MogakStudy/Util/setFont.swift
@@ -7,28 +7,36 @@
 
 import UIKit
 
+enum NotoSansStyle: String {
+    case regular = "NotoSansCJKkr-Regular"
+    case medium = "NotoSansCJKkr-Medium"
+}
+
 extension UIFont {
     // Title
-    static var Display1_R20: UIFont { customFont("NotoSansCJKkr-Regular", size: 20.0) }
+    static var Onboarding_M24: UIFont { customFont(NotoSansStyle.medium.rawValue, size: 24)}
+    static var Onboarding_R24: UIFont { customFont(NotoSansStyle.regular.rawValue, size: 24)}
+    
+    static var Display1_R20: UIFont { customFont(NotoSansStyle.regular.rawValue, size: 20.0) }
     static var Title1_M16: UIFont {
-        customFont("NotoSansCJKkr-Medium", size: 16.0) }
-    static var Title2_R16: UIFont { customFont("NotoSansCJKkr-Regular", size: 16.0) }
+        customFont(NotoSansStyle.medium.rawValue, size: 16.0) }
+    static var Title2_R16: UIFont { customFont(NotoSansStyle.regular.rawValue, size: 16.0) }
     static var Title3_M14: UIFont {
-        customFont("NotoSansCJKkr-Medium", size: 14) }
-    static var Title4_R14: UIFont { customFont("NotoSansCJKkr-Regular", size: 14) }
+        customFont(NotoSansStyle.medium.rawValue, size: 14) }
+    static var Title4_R14: UIFont { customFont(NotoSansStyle.regular.rawValue, size: 14) }
     static var Title5_M12: UIFont {
-        customFont("NotoSansCJKkr-Medium", size: 14) }
-    static var Title6_R12: UIFont { customFont("NotoSansCJKkr-Regular", size: 12.0) }
+        customFont(NotoSansStyle.medium.rawValue, size: 14) }
+    static var Title6_R12: UIFont { customFont(NotoSansStyle.regular.rawValue, size: 12.0) }
     
     // Body
     static var Body1_M16: UIFont  {
-        customFont("NotoSansCJKkr-Medium", size: 16.0) }
-    static var Body2_R16: UIFont  { customFont("NotoSansCJKkr-Regular", size: 16.0) }
-    static var Body3_R14: UIFont  { customFont("NotoSansCJKkr-Regular", size: 14.0) }
-    static var Body4_R12: UIFont  { customFont("NotoSansCJKkr-Regular", size: 12.0) }
+        customFont(NotoSansStyle.medium.rawValue, size: 16.0) }
+    static var Body2_R16: UIFont  { customFont(NotoSansStyle.regular.rawValue, size: 16.0) }
+    static var Body3_R14: UIFont  { customFont(NotoSansStyle.regular.rawValue, size: 14.0) }
+    static var Body4_R12: UIFont  { customFont(NotoSansStyle.regular.rawValue, size: 12.0) }
     
     // Caption
-    static var caption_R10: UIFont  { customFont("NotoSansCJKkr-Regular", size: 10.0) }
+    static var caption_R10: UIFont  { customFont(NotoSansStyle.regular.rawValue, size: 10.0) }
     
     /// 커스텀 폰트를 설정하는 메서드
     private static func customFont(
