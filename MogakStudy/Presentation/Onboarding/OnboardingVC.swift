25e74f35b46ee19246f2544a62ba60566a2a43f7
Doy Kim <kimdee.dev@gmail.com>
2022-11-14 20:24:35 +0900

    [Add] #30 Onboarding View, VC 추가

---
+++ MogakStudy/Presentation/Onboarding/OnboardingVC.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingVC.swift
+//  MogakStudy
+//
+//  Created by Doy Kim on 2022/11/14.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ MogakStudy/Presentation/Onboarding/OnboardingView.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingView.swift
+//  MogakStudy
+//
+//  Created by Doy Kim on 2022/11/14.
+//
+
+import UIKit
+
+class OnboardingView: UIView {
+
+
+
+}
