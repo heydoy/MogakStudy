e2d703b501b8ae4232ce8df52860822f33be869c
Doy Kim <kimdee.dev@gmail.com>
2022-11-14 14:34:34 +0900

    [Add] #25 스트링 리터럴 수정 및 회원가입 리터럴용 변수 추가

--- MogakStudy/Global/Literal/I18NStrings.swift
+++ MogakStudy/Global/Literal/I18NStrings.swift
@@ -11,18 +11,45 @@ struct I18NStrings {
     struct Common {
         static let appName = "appName".localized
         static let ok = "ok".localized
-        static let start = "start".localized
+        static let cancel = "cancel".localized
     }
     struct Onboarding {
         static let firstComment = "firstComment".localized
         static let secondComment = "secondComment".localized
+        static let thirdComment = "thirdComment".localized
+        
+        static let attributedFirstComment = "attributedFirsteComment".localized
+        static let attributedSecondComment = "attributedSecondComment".localized
+        
+        static let startButtonTitle = "startButtonTitle".localized
     }
     struct SMSAuth {
         static let pleaseEnterPhoneNumber = "pleaseEnterPhoneNumber".localized
         static let phoneNumberInputPlaceholder = "phoneNumberInputPlaceholder".localized
         static let phoneNumberButtonTitle = "phoneNumberButtonTitle".localized
         static let authSMSSent = "AuthSMSSent".localized
+        static let authCodeInputPlaceholder = "authCodeInputPlaceholder".localized
         static let resendButtonTitle = "resendButtonTitle".localized
         static let authAndStartButtonTitle = "authAndStartButtonTitle".localized
+    }
+    struct Signup {
+        static let nextButtonTitle = "nextButtonTitle".localized
+
+        static let pleaseEnterNickname = "pleaseEnterNickname".localized
+        static let nicknameInputPlaceholder = "nicknameInputPlaceholder".localized
+        
+        static let pleaseEnterBirthday = "pleaseEnterBirthday".localized
+        static let year = "year".localized
+        static let month = "month".localized
+        static let day = "day".localized
+        
+        static let pleaseEnterEmail = "pleaseEnterEmail".localized
+        static let neededToChangePhoneNumber = "neededToChangePhoneNumber".localized
+        static let emailInputPlaceholer = "emailInputPlaceholer".localized
+        
+        static let pleaseSelectGener = "pleaseSelectGener".localized
+        static let neededToFindSesac = "neededToFindSesac".localized
+        static let male = "male".localized
+        static let female = "female".localized
     }
 }
