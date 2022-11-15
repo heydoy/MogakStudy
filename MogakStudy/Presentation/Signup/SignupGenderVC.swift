09e216e0910f753894ba90b2957d5d0b0629f1ec
Doy Kim <kimdee.dev@gmail.com>
2022-11-16 03:37:54 +0900

    [Design] #39 성별선택 화면 디자인

---
+++ MogakStudy/Presentation/Signup/SignupGenderVC.swift
@@ -0,0 +1,20 @@
+//
+//  SignupGenderVC.swift
+//  MogakStudy
+//
+//  Created by Doy Kim on 2022/11/16.
+//
+
+import UIKit
+
+class SignupGenderVC: BaseVC {
+    
+    let mainView = SignupGenderView()
+    
+    override func loadView() {
+        view = mainView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
