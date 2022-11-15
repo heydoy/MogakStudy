ddcbfbf25017a512eb2155883a53fd407c09ac51
Doy Kim <kimdee.dev@gmail.com>
2022-11-16 03:20:28 +0900

    [Design] #39 회원가입 이메일 화면 디자인

---
+++ MogakStudy/Presentation/Signup/SignupEmailVC.swift
@@ -0,0 +1,20 @@
+//
+//  SignupEmailVC.swift
+//  MogakStudy
+//
+//  Created by Doy Kim on 2022/11/16.
+//
+
+import UIKit
+
+class SignupEmailVC: BaseVC {
+    
+    let mainView = SignupEmailView()
+    
+    override func loadView() {
+        view = mainView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
