2f4e451e840a8714ef5080227f4bcd6fc9ca8506
Doy Kim <kimdee.dev@gmail.com>
2022-11-15 18:03:24 +0900

    [Feature] #39 회원가입 뷰, VC 추가

---
+++ MogakStudy/Presentation/Signup/SignupView.swift
@@ -0,0 +1,19 @@
+//
+//  SignupView.swift
+//  MogakStudy
+//
+//  Created by Doy Kim on 2022/11/15.
+//
+
+import UIKit
+
+class SignupView: BaseView {
+    
+    override func setupUI() {
+        super.setupUI()
+    }
+    
+    override func setConstraints() {
+        
+    }
+}
