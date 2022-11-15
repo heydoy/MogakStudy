723f37700917d445db8bdf41a73e12cc89ecd400
Doy Kim <kimdee.dev@gmail.com>
2022-11-16 03:20:12 +0900

    [Design] #39 회원가입 생일 화면 디자인

---
+++ MogakStudy/Presentation/Signup/SignupBirthdayVC.swift
@@ -0,0 +1,20 @@
+//
+//  SignupBirthdayVC.swift
+//  MogakStudy
+//
+//  Created by Doy Kim on 2022/11/16.
+//
+
+import UIKit
+
+class SignupBirthdayVC: BaseVC {
+    
+    let mainView = SignupBirthdayView()
+    
+    override func loadView() {
+        view = mainView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ MogakStudy/Presentation/Signup/SignupBirthdayView.swift
@@ -0,0 +1,101 @@
+//
+//  SignupBirthdayView.swift
+//  MogakStudy
+//
+//  Created by Doy Kim on 2022/11/16.
+//
+
+import UIKit
+import Then
+import SnapKit
+import MogakStudyUIFramework
+
+class SignupBirthdayView: BaseView {
+    let guideLabel = UILabel().then {
+        $0.font = .Display1_R20
+        $0.textColor = .msColor.black
+        $0.text = I18NStrings.Signup.pleaseEnterBirthday
+        $0.textAlignment = .center
+        $0.numberOfLines = 0
+    }
+    var stackView = UIStackView()
+    var yearStackView = UIStackView()
+    var monthStackView = UIStackView()
+    var dayStackView = UIStackView()
+    
+    let birthYInput = msInput(status: InactiveInput()).then {
+        $0.placeholder = "1990"
+        $0.font = .Title4_R14
+        $0.isUserInteractionEnabled = false
+    }
+    let yearLabel = UILabel().then {
+        $0.text = I18NStrings.Signup.year
+        $0.font = .Title2_R16
+    }
+    let birthMInput = msInput(status: InactiveInput()).then {
+        $0.placeholder = "1"
+        $0.font = .Title4_R14
+        $0.isUserInteractionEnabled = false
+    }
+    let monthLabel = UILabel().then {
+        $0.text = I18NStrings.Signup.month
+        $0.font = .Title2_R16
+    }
+    let birthDInput = msInput(status: InactiveInput()).then {
+        $0.placeholder = "1"
+        $0.font = .Title4_R14
+        $0.isUserInteractionEnabled = false
+    }
+    let dayLabel = UILabel().then {
+        $0.text = I18NStrings.Signup.day
+        $0.font = .Title2_R16
+    }
+    
+    let nextButton = msButton(status: DisableButton()).then {
+        $0.setTitle(I18NStrings.Signup.nextButtonTitle, for: .normal)
+    }
+    
+    override func setupUI() {
+        super.setupUI()
+        
+        yearStackView.addArangedSubviews(birthYInput, yearLabel)
+        monthStackView.addArangedSubviews(birthMInput, monthLabel)
+        dayStackView.addArangedSubviews(birthDInput, dayLabel)
+        
+        stackView.addArangedSubviews(
+            yearStackView, monthStackView, dayStackView
+        )
+        
+        addSubviews(guideLabel, stackView, nextButton)
+        
+        // 스택뷰 설정
+        stackView.spacing = 23
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        
+        // 작은 스택뷰 설정
+        [ yearStackView, monthStackView, dayStackView ].forEach {
+            $0.spacing = 4
+            $0.axis = .horizontal
+            $0.distribution = .fillProportionally
+        }
+    }
+    
+    override func setConstraints() {
+        nextButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(48)
+            make.bottom.equalTo(safeAreaLayoutGuide).inset(340)
+        }
+        stackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(48)
+            make.bottom.equalTo(nextButton.snp.top).offset(-32)
+        }
+        guideLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(stackView.snp.top).offset(-40)
+        }
+    }
+    
+}
