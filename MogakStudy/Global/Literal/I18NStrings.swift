3636bddeeb9a0c6508b08ca8895d73c370347da8
Doy Kim <kimdee.dev@gmail.com>
2022-11-19 23:11:45 +0900

    [Design] #47 내정보, 정보관리에 들어가는 문자열 리터럴 정의

--- MogakStudy/Global/Literal/I18NStrings.swift
+++ MogakStudy/Global/Literal/I18NStrings.swift
@@ -61,4 +61,34 @@ struct I18NStrings {
     struct Home {
         static let whole = "whole".localized
     }
+    struct MyInfo {
+        // 내정보
+        static let notice = "notice".localized
+        static let faq = "faq".localized
+        static let qna = "qna".localized
+        static let alertSetting = "alertSetting".localized
+        static let permit = "permit".localized
+        
+        // 정보관리
+        static let infoManagement = "infoManagement".localized
+        static let save = "save".localized
+        static let myGender = "myGender".localized
+        static let studyAlot = "studyAlot".localized
+        static let pleaseEnterStudyPlaceholder = "pleaseEnterStudyPlaceholder".localized
+        static let allowSearchMynumber = "allowSearchMynumber".localized
+        static let ageSpan = "ageSpan".localized
+        static let withdraw = "withdraw".localized
+        
+        // 펼쳐졌을 때 내용
+        static let sesacTitle = "sesacTitle".localized
+        static let goodManner = "goodManner".localized
+        static let punctuality = "punctuality".localized
+        static let fastResponse = "fastResponse".localized
+        static let kindPersonality = "kindPersonality".localized
+        static let proficiency = "proficiency".localized
+        static let beneficialTime = "beneficialTime".localized
+        
+        static let sesacReview = "sesacReview".localized
+        static let waitingForFirstReviewPlaceholder = "waitingForFirstReviewPlaceholder".localized
+    }
 }
